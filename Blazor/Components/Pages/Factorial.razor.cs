3ad6c4f14606595576fec3d564bef13b22ea3893
alex <kond9.k@yandex.ru>
2026-04-05 12:04:56 +0300

    Factorial split done.

--- Blazor/Components/Pages/Factorial.razor.cs
+++ Blazor/Components/Pages/Factorial.razor.cs
@@ -1,6 +1,19 @@
-﻿namespace Blazor.Components.Pages
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
 {
-    public class Factorial
+    public partial class Factorial
     {
+        int n;
+        BigInteger factorial;
+        void Calculate()
+        {
+            factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+        }
     }
 }
