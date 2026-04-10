8dbd77582798d81d1151402e0f30150ca08a64c3
alex <kond9.k@yandex.ru>
2026-04-10 07:45:15 +0300

    ДЗ сделано.

---
+++ Blazor/Components/Pages/Fibonacci.razor.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+    public partial class Fibonacci
+    {
+         int count = 10;
+         List<BigInteger> fibonacciNumbers = new();
+
+        private void GenerateFibonacci()
+        {
+            fibonacciNumbers.Clear();
+            for (int i = 0; i < count; i++)
+                fibonacciNumbers.Add(FibonacciRecursive(i));
+        }
+
+        private BigInteger FibonacciRecursive(int n)
+        {
+            if (n <= 1) return n;
+            return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+        }
+    }
+    
+}
---
+++ Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+    public partial class Power
+    {
+        int num;
+        int pow;
+        BigInteger result;
+        void Calculate()
+        {
+            result=(BigInteger)Math.Pow(num, pow);
+        }
+    }
+}
