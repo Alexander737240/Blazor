c604fa52b12618c3a1e03a72d1fe5f19067d167c
alex <kond9.k@yandex.ru>
2026-04-22 08:11:42 +0300

    Movies DONE.

---
+++ Movies/Migrations/20260422044420_mssql.local_migration_682.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Movies.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssqllocal_migration_682 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Poster",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "URL",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Poster",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "URL",
+                table: "Movie");
+        }
+    }
+}
