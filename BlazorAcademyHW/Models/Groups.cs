8252198b7da9363fe02e703f1e6be0952f957758
alex <kond9.k@yandex.ru>
2026-05-13 08:45:24 +0300

    Groups and Students changes.

--- BlazorAcademyHW/Models/Groups.cs
+++ BlazorAcademyHW/Models/Groups.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorAcademyHW.Models
 {
+
     public class Groups
     {
        [Key] public int group_id { get; set; }
@@ -9,5 +11,7 @@ namespace BlazorAcademyHW.Models
         public string? group_name { get; set; }
 
         public byte ? direction { get; set; }
+        //public Disciplines Discipline { get; set; }  // Навигационное свойств
     }
 }
+
